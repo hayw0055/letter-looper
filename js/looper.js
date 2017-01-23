74f4dd74bfdfa0f9abd9ebd68d622d914df756ee
Stephanie Hayward Johns <hayw0055@algonquinlive.com>
2017-01-23 12:54:19 -0500

    created js doc

---
+++ js/looper.js
@@ -0,0 +1,14 @@
+var letter;
+var i;
+var k;
+
+while(!letter){
+     letter = prompt("Enter a Letter");
+    for ( i = 0; i < 10; i += 1) {
+        document.write("<br>")
+    for ( k = 0; k < i; k += 1) {
+        document.write (letter)
+  
+} 
+} 
+}
