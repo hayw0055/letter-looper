7995c6f0a5a29bb8de5b51efd6bd3bf70757bd01
Stephanie Hayward Johns <hayw0055@algonquinlive.com>
2017-01-23 13:33:32 -0500

    Update looper.js

--- js/looper.js
+++ js/looper.js
@@ -4,7 +4,7 @@ var k;
 
 while(!letter){
      letter = prompt("Enter a Letter");
-    for ( i = 0; i < 10; i += 1) {
+for ( i = 0; i < 10; i += 1) {
         document.write("<br>")
     for ( k = 0; k < i; k += 1) {
         document.write (letter)
